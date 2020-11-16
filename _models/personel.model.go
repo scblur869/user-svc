2f8df913d925b79a2b93663438be86c52715fa40
Brad Ross (FG-AM-63) <Brad.Ross@bmwmc.com>
2020-11-16 08:21:06 -0500

    adjusted case

--- _models/personel.model.go
+++ _models/personel.model.go
@@ -2,18 +2,18 @@ package _models
 
 type EMPLOYEE struct {
 	Id             int    `json:"id"`
-	First_Name     string `json:"first_name"`
-	Last_Name      string `json:"last_name"`
-	Street_Address string `json:"street_address"`
+	First_Name     string `json:"firstname"`
+	Last_Name      string `json:"lastname"`
+	Street_Address string `json:"streetaddress"`
 	Gender         string `json:"gender"`
 	Email          string `json:"email"`
-	Personel_Id    string `json:"personel_id"`
+	Personel_Id    string `json:"personelid"`
 	Department     string `json:"department"`
 	Phone          string `json:"phone"`
 	Birthdate      string `json:"birthdate"`
-	Photo_Id       string `json:"photo_id"`
-	Attribute_1    string `json:"attribute_1"`
-	Attribute_2    string `json:"attribute_2"`
+	Photo_Id       string `json:"photoid"`
+	Attribute_1    string `json:"attribute1"`
+	Attribute_2    string `json:"attribute2"`
 }
 
 type ID struct {
