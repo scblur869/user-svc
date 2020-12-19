12fa07cc2c5a6e13dee0649c30db1a4a8b7526b8
Brad Ross (FG-AM-63) <Brad.Ross@bmwmc.com>
2020-12-19 09:24:58 -0500

    updated cors policy

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"local/user-svc/_data"
 	"local/user-svc/_services"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,11 +16,19 @@ func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	// routes
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	r.GET("/api/v1/get-all", _services.GetFullEmployeeList)
 	r.GET("/api/v1/get-by-id/:id", _services.GetEmployeeById)
 	r.POST("/api/v1/remove-user", _services.RemoveEmployeeById) // accepts registration request from the camera
 	r.POST("/api/v1/add-user", _services.ImportRequest)
 	r.POST("/api/v1/update-user", _services.UpdateEmployeeData)
-	r.Use(cors.Default())
+
 	r.Run("0.0.0.0:8082") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
