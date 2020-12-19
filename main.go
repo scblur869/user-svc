113eab367162c6ecf1d26f9533685e350e2581fd
Brad Ross (FG-AM-63) <Brad.Ross@bmwmc.com>
2020-12-19 09:37:35 -0500

    updated cors

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"POST", "GET"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
