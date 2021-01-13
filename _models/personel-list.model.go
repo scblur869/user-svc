0370f635f2ca40fa53b2833df1cba4ee89a7db12
Brad Ross (FG-AM-63) <Brad.Ross@bmwmc.com>
2021-01-13 10:20:52 -0500

    updated case in request model

--- _models/personel-list.model.go
+++ _models/personel-list.model.go
@@ -24,59 +24,59 @@ type PersonList struct {
 }
 
 type PersonListRequest struct {
-	Name      string                `json:"name"`
-	UUID      string                `json:"uuid"`
-	Session   string                `json:"session"`
-	Timestamp int                   `json:"timestamp"`
-	Sign      string                `json:"sign"`
-	Data      PersonListRequestData `json:"data"`
+	Name      string                `json:"Name"`
+	UUID      string                `json:"UUID"`
+	Session   string                `json:"Session"`
+	Timestamp int                   `json:"TimeStamp"`
+	Sign      string                `json:"Sign"`
+	Data      PersonListRequestData `json:"Data"`
 }
 
 type PersonListRequestData struct {
-	Action      string     `json:"action"`
-	PersonType  int        `json:"persontype"`
-	FeatureInfo FeatureSub `json:"featureinfo"`
-	PersonInfo  PersonInfo `json:"personinfo"`
+	Action      string     `json:"Action"`
+	PersonType  int        `json:"PersonType"`
+	FeatureInfo FeatureSub `json:"FeatureInfo"`
+	PersonInfo  PersonInfo `json:"PersonInfo"`
 }
 
 type FeatureSub struct {
-	FeatureType int `json:"featuretype"`
+	FeatureType int `json:"FeatureType"`
 }
 
 type PersonInfo struct {
-	PersonCover     int             `json:"personcover"`
-	PersonId        string          `json:"personid"`
-	PersonName      string          `json:"personname"`
-	Sex             int             `json:"sex"`
-	IDCard          string          `json:"idcard"`
-	Nation          string          `json:"nation"`
-	Birthday        string          `json:"birthday"`
-	Phone           string          `json:"phone"`
-	Address         string          `json:"address"`
-	LimitTime       int             `json:"limittime"`
-	StartTime       string          `json:"starttime"`
-	EndTime         string          `json:"endtime"`
-	Label           string          `json:"label"`
-	PersonExtension PersonExtension `json:"personextension"`
-	PersonPhoto     string          `json:"personphoto"`
-	FeatureValue    string          `json:"featurevalue"`
+	PersonCover     int             `json:"PersonCover"`
+	PersonId        string          `json:"PersonId"`
+	PersonName      string          `json:"PersonName"`
+	Sex             int             `json:"Sex"`
+	IDCard          string          `json:"IDCard"`
+	Nation          string          `json:"Nation"`
+	Birthday        string          `json:"Birthday"`
+	Phone           string          `json:"Phone"`
+	Address         string          `json:"Address"`
+	LimitTime       int             `json:"LimitTime"`
+	StartTime       string          `json:"StartTime"`
+	EndTime         string          `json:"EndTime"`
+	Label           string          `json:"Label"`
+	PersonExtension PersonExtension `json:"PersonExtension"`
+	PersonPhoto     string          `json:"PersonPhoto"`
+	FeatureValue    string          `json:"FeatureValue"`
 }
 
 type PersonExtension struct {
-	PersonCode1       string `json:"personcode1"`
-	PersonCode2       string `json:"personcode2"`
-	PersonCode3       string `json:"personcode3"`
-	PersonReserveName string `json:"personreservename"`
-	PersonParam1      int    `json:"personparam1"`
-	PersonParam2      int    `json:"personparam2"`
-	PersonParam3      int    `json:"personparam3"`
-	PersonParam4      int    `json:"personparam4"`
-	PersonParam5      int    `json:"personparam5"`
-	PersonData1       string `json:"persondata1"`
-	PersonData2       string `json:"persondata2"`
-	PersonData3       string `json:"persondata3"`
-	PersonData4       string `json:"persondata4"`
-	PersonData5       string `json:"persondata5"`
+	PersonCode1       string `json:"PersonCode1"`
+	PersonCode2       string `json:"PersonCode2"`
+	PersonCode3       string `json:"PersonCode3"`
+	PersonReserveName string `json:"PersonReserveName"`
+	PersonParam1      int    `json:"PersonParam1"`
+	PersonParam2      int    `json:"PersonParam2"`
+	PersonParam3      int    `json:"PersonParam3"`
+	PersonParam4      int    `json:"PersonParam4"`
+	PersonParam5      int    `json:"PersonParam5"`
+	PersonData1       string `json:"PersonData1"`
+	PersonData2       string `json:"PersonData2"`
+	PersonData3       string `json:"PersonData3"`
+	PersonData4       string `json:"PersonData4"`
+	PersonData5       string `json:"PersonData5"`
 }
 
 type DEVICE struct {
